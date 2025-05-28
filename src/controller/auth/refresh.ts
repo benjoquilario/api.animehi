99ab787f16a4366590b029b2ffd248f9843b1c57
benjoquilario <benjoquilario@gmail.com>
2025-05-28 20:10:27 +0800

    feat: refresh token

---
+++ src/controller/auth/refresh.ts
@@ -0,0 +1,56 @@
+import type { Context } from "hono"
+import { getCookie } from "hono/cookie"
+import { verifyJwt } from "../../utils/session"
+import db from "../../config/database"
+import jwt from "jsonwebtoken"
+import { env } from "../../config/env"
+
+export const refresh = async (c: Context) => {
+  // check if the refresh token is valid
+  const token = getCookie(c, "refreshToken") as string
+
+  try {
+    const decoded = verifyJwt(token)
+
+    const user = await db.user.findUnique({
+      where: {
+        id: decoded.sub as string,
+      },
+    })
+
+    if (!user) {
+      return c.json({ error: "User not found" }, 400)
+    }
+
+    const account = await db.account.findFirst({
+      where: {
+        userId: user.id,
+      },
+    })
+
+    if (!account) {
+      return c.json({ error: "Account not found" }, 400)
+    }
+
+    const accessToken = jwt.sign(
+      { sub: user.id, name: user.name },
+      env.JWT_SECRET,
+      { expiresIn: "2h" }
+    )
+
+    // res.send({ accessToken, user });
+    return c.json(
+      {
+        message: "Token refreshed",
+        user: {
+          token: account.access_token,
+          accessToken,
+          user,
+        },
+      },
+      { status: 200 }
+    )
+  } catch (error) {
+    return c.json({ error: "Invalid token" }, { status: 400 })
+  }
+}
