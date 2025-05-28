4616a7f002506858634fa6e37543ff7387f63128
benjoquilario <benjoquilario@gmail.com>
2025-05-28 19:53:20 +0800

    feat: get access_token, refresh_token and token type from oauth anilist token

---
+++ src/services/anilist.ts
@@ -0,0 +1,73 @@
+import { config } from "dotenv"
+
+config()
+
+export interface AnilistAuth {
+  token_type: string
+  expires_in: number
+  access_token: string
+  refresh_token: string
+}
+
+export interface AnilistUser {
+  id: string
+  username: string
+  image_url: string
+  email: string
+}
+
+export const getAnilistAuth = async (code: string) => {
+  const res = await fetch("https://anilist.co/api/v2/oauth/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      grant_type: "authorization_code",
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      redirect_uri: process.env.REDIRECT_URL, // http://your-api-url/api/auth/anilist/callback
+      code: code, // The Authorization Code received previously
+    }),
+  })
+
+  const { access_token, refresh_token, token_type } =
+    (await res.json()) as AnilistAuth
+
+  return { access_token, refresh_token, token_type }
+}
+
+export const getAnilistUser = async (accessToken: string) => {
+  const response = await fetch("https://graphql.anilist.co", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query: `query {
+                Viewer {
+                  id
+                  name
+                  avatar {
+                    large
+                    medium
+                  }
+                }
+              }
+            `,
+    }),
+  })
+  const data = (await response.json()) as any
+
+  const { id, name, avatar } = data.data.Viewer
+
+  return {
+    id: String(id),
+    username: name,
+    image_url: avatar.large,
+    email: `${name}@gmail.com`,
+  }
+}
