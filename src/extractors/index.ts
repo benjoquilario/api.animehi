6b2ed9168096b3faa8d9a7a9d17c3d191b9478d9
benjoquilario <benjoquilario@gmail.com>
2025-05-30 15:54:03 +0800

    feat: added extractors for hianimez

---
+++ src/extractors/index.ts
@@ -0,0 +1,6 @@
+import StreamSB from "./streamsb"
+import StreamTape from "./streamtape"
+import RapidCloud from "./rapidcloud"
+import MegaCloud from "./megacloud"
+
+export { StreamSB, StreamTape, RapidCloud, MegaCloud }
