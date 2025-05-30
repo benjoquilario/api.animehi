7c36902609a65501d5a086afb46138bd49d31d25
benjoquilario <benjoquilario@gmail.com>
2025-05-30 15:59:25 +0800

    feat: added strictRateLimiting for other route use

--- src/config/ratelimit.ts
+++ src/config/ratelimit.ts
@@ -1,12 +1,26 @@
-import { config } from "dotenv"
 import { rateLimiter } from "hono-rate-limiter"
 import { getConnInfo } from "@hono/node-server/conninfo"
-
-config()
+import { env } from "./env"
 
 export const ratelimit = rateLimiter({
-  windowMs: Number(process.env.WINDOW_MS) || 30 * 60 * 1000,
-  limit: Number(process.env.MAX_REQS) || 5,
+  windowMs: Number(env.WINDOW_MS) || 30 * 60 * 1000,
+  limit: Number(env.MAX_REQS) || 5,
+  standardHeaders: "draft-7",
+  keyGenerator(c) {
+    const { remote } = getConnInfo(c)
+    const key =
+      `${String(remote.addressType)}_` +
+      `${String(remote.address)}:${String(remote.port)}`
+
+    return key
+  },
+  handler: (c) =>
+    c.json({ status: 429, message: "Too Many Requests 😵" }, { status: 429 }),
+})
+
+export const strictRatelimit = rateLimiter({
+  windowMs: Number(env.WINDOW_MS) || 30 * 60 * 1000,
+  limit: Number(env.MAX_REQS) || 5,
   standardHeaders: "draft-7",
   keyGenerator(c) {
     const { remote } = getConnInfo(c)
