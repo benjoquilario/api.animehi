63ba54081f7a1f8b880c87efa6a83884682a7a08
benjoquilario <benjoquilario@gmail.com>
2025-05-30 16:22:42 +0800

    feat: added auth route

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import type { APIVariables } from "./config/variables"
 import { Hono } from "hono"
 import corsConfig from "./config/cors"
 import { serve } from "@hono/node-server"
-import { cacheControlMiddleware } from "./middleware/cache"
+import { cacheControlMiddleware, validateApiKey } from "./middleware/cache"
 import { ratelimit } from "./config/ratelimit"
 import { cacheConfigSetter } from "./middleware/cache"
 import anilistRouter from "./routes/meta/anilist"
@@ -12,9 +12,10 @@ import zoroRouter from "./routes/anime/zoro"
 import { logging } from "./middleware/logger"
 import { log } from "./config/logger"
 import { env } from "./config/env"
+import { authRouter } from "./routes/auth/anilist"
 
 const BASE_PATH = "/api" as const
-const PORT: number = Number(env.PORT) || 4000
+const PORT: number = env.PORT
 const HOSTNAME = env.HOSTNAME
 const VERCEL_DEPLOYMENT = env.VERCEL_DEPLOYMENT
 
@@ -40,26 +41,33 @@ app.basePath(BASE_PATH).route("/meta/anilist", anilistRouter)
 app.basePath(BASE_PATH).route("/anime/zoro", zoroRouter)
 app.basePath(BASE_PATH).route("/anime/animekai", animekaiRouter)
 
-if (!Boolean(VERCEL_DEPLOYMENT)) {
-  serve({
-    port: PORT,
-    fetch: app.fetch,
-  }).addListener("listening", () => {
-    log.info(`api.animehi RUNNING at http://localhost:${PORT}`)
-  })
+// auth routes
+app.basePath(BASE_PATH).route("/auth", authRouter)
 
-  // NOTE: remove the `if` block below for personal deployments
-  if (ISNT_PERSONAL_DEPLOYMENT) {
-    const interval = 9 * 60 * 1000 // 9mins
+const server = function () {
+  if (!Boolean(VERCEL_DEPLOYMENT)) {
+    serve({
+      port: PORT,
+      fetch: app.fetch,
+    }).addListener("listening", () => {
+      log.info(`api.animehi RUNNING at http://localhost:${PORT}`)
+    })
 
-    // don't sleep
-    setInterval(() => {
-      log.info("api.animehi HEALTH_CHECK at", new Date().toISOString())
-      https.get(`https://${HOSTNAME}/health`).on("error", (err) => {
-        log.error(err.message.trim())
-      })
-    }, interval)
+    // NOTE: remove the `if` block below for personal deployments
+    if (ISNT_PERSONAL_DEPLOYMENT) {
+      const interval = 9 * 60 * 1000 // 9mins
+
+      // don't sleep
+      setInterval(() => {
+        log.info("api.animehi HEALTH_CHECK at", new Date().toISOString())
+        https.get(`https://${HOSTNAME}/health`).on("error", (err) => {
+          log.error(err.message.trim())
+        })
+      }, interval)
+    }
   }
 }
+
+server()
 
 export default app
