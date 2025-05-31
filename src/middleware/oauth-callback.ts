1f392dfb7dfdd41a8a628a89ed98700e406e8bc0
benjoquilario <benjoquilario@gmail.com>
2025-05-31 21:51:53 +0800

    feat: refactor oAuthCallback to use createMiddleware for improved structure

--- src/middleware/oauth-callback.ts
+++ src/middleware/oauth-callback.ts
@@ -1,27 +1,31 @@
 import type { Context, Next } from "hono"
 import { getAnilistAuth, getAnilistUser } from "../services/anilist"
+import { createMiddleware } from "hono/factory"
 
-export const oAuthCallback = async (c: Context, next: Next) => {
-  try {
-    // get the code from the query parameters "code" the ones that anilist sends back
-    const { access_token, refresh_token, token_type } = await getAnilistAuth(
-      c.req.query("code")!
-    )
+export const oAuthCallback = createMiddleware(
+  async (c: Context, next: Next) => {
+    try {
+      const code = c.req.query("code") as string
 
-    // we need the access token from getAnilist to get the anilist user data
-    const userData = await getAnilistUser(access_token)
+      // get the code from the query parameters "code" the ones that anilist sends back
+      const { access_token, refresh_token, token_type } =
+        await getAnilistAuth(code)
 
-    // set the user data in the context for later use including the access token
-    // and refresh token
-    c.set("anilistUser", {
-      ...userData,
-      access_token,
-      refresh_token,
-      token_type,
-    })
+      // we need the access token from getAnilist to get the anilist user data
+      const userData = await getAnilistUser(access_token)
 
-    await next()
-  } catch (error) {
-    return c.json({ error: "Access token is not valid" }, { status: 401 })
+      // set the user data in the context for later use including the access token
+      // and refresh token
+      c.set("anilistUser", {
+        ...userData,
+        access_token,
+        refresh_token,
+        token_type,
+      })
+
+      await next()
+    } catch (error) {
+      return c.json({ error: "Access token is not valid" }, { status: 401 })
+    }
   }
-}
+)
